8a9c0cd4b6ab5577e392210e9e6de6a26d2eb517
Dennis Renz <dennis@r-renz.de>
2021-01-10 13:24:23 +0100

    Initial commit

---
+++ Malefics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Malefics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
