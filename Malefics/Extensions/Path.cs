da89704c37284fea48e767add160887ee2f31c90
Dennis Renz <dennis@r-renz.de>
2021-01-11 19:39:30 +0100

    Add function for axis parallel paths

--- Malefics/Extensions/Path.cs
+++ Malefics/Extensions/Path.cs
@@ -1,4 +1,5 @@
 ﻿using Malefics.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,6 +18,25 @@ namespace Malefics.Extensions
                 .Aggregate(
                     true,
                     (IsPathUntilPq, pq) => IsPathUntilPq && (pq.First.IsNeighborOf(pq.Second)));
+        }
+
+        public static IEnumerable<Position> AxisParallel(Position start, Position end)
+        {
+            if (start.X != end.X)
+                return Enumerable
+                    .Range(0, Math.Abs(start.X - end.X))
+                    .Select(x => start.X < end.X ? x : -x)
+                    .Select(x => new Position(start.X + x, start.Y))
+                    .Append(end);
+
+            if (start.Y != end.Y)
+                return Enumerable
+                    .Range(0, Math.Abs(start.Y - end.Y))
+                    .Select(y => start.Y < end.Y ? y : -y)
+                    .Select(y => new Position(start.X, start.Y + y))
+                    .Append(end);
+
+            throw new ArgumentException($"{start} and {end} have no equal coordinate");
         }
     }
 }
---
+++ MaleficsTest/Extensions/PathTest.cs
@@ -0,0 +1,29 @@
+﻿using Malefics.Extensions;
+using Malefics.Models;
+using NUnit.Framework;
+
+namespace MaleficsTest.Extensions
+{
+    public class PathTest
+    {
+        [Test]
+        public void Axis_Parallel_Paths_In_All_Directions()
+        {
+            var path = Path.AxisParallel(new(0, 0), new(2, 0));
+            Assert.That(path, Is.EquivalentTo(new[] {
+                new Position(0, 0), new(1, 0), new(2, 0) }));
+
+            path = Path.AxisParallel(new(0, 0), new(0, -2));
+            Assert.That(path, Is.EquivalentTo(new[] {
+                new Position(0, 0), new(0, -1), new(0, -2) }));
+
+            path = Path.AxisParallel(new(-1, -1), new(-3, -1));
+            Assert.That(path, Is.EquivalentTo(new[] {
+                new Position(-1, -1), new(-2, -1), new(-3, -1) }));
+
+            path = Path.AxisParallel(new(-1, -1), new(-1, 1));
+            Assert.That(path, Is.EquivalentTo(new[] {
+                new Position(-1, -1), new(-1, 0), new(-1, 1) }));
+        }
+    }
+}
