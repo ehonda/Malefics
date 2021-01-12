42b13ff11b58befd1e0d0a097fac839379108bd2
Dennis Renz <dennis@r-renz.de>
2021-01-12 20:18:16 +0100

    Add tile parser

---
+++ Malefics/Parsers/Ascii/TileParser.cs
@@ -0,0 +1,26 @@
+﻿using Malefics.Models;
+using Malefics.Models.Pieces;
+using System;
+
+namespace Malefics.Parsers.Ascii
+{
+    public class TileParser
+    {
+        private const char ROCK = ' ';
+        private const char ROAD = '.';
+        private const char BARRICADE = 'o';
+
+        public Tile Parse(char tile)
+            => tile switch
+            {
+                ROCK => new() { Terrain = Terrain.Rock },
+                ROAD => new() { Terrain = Terrain.Road },
+                BARRICADE => new() 
+                {
+                    Terrain = Terrain.Road, 
+                    OccupyingPiece = new Barricade() 
+                },
+                _ => throw new ArgumentException($"Unkown tile encoding: {tile}")
+            };
+    }
+}
