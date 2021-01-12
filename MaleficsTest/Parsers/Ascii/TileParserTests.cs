26724e7268991eb1abb4cc79592466310e6646fd
Dennis Renz <dennis@r-renz.de>
2021-01-12 20:33:26 +0100

    Add tile parser tests

---
+++ MaleficsTest/Parsers/Ascii/TileParserTests.cs
@@ -0,0 +1,29 @@
+﻿using Malefics.Models;
+using Malefics.Models.Pieces;
+using Malefics.Parsers.Ascii;
+using NUnit.Framework;
+
+namespace MaleficsTest.Parsers.Ascii
+{
+    [TestFixture]
+    public class TileParserTests
+    {
+        private TileParser _parser = new();
+
+        [SetUp]
+        public void Setup()
+        {
+            _parser = new();
+        }
+
+        [Test]
+        public void A_Barricade_Is_Parsed_With_A_Road_Tile()
+        {
+            var tile = _parser.Parse('o');
+            Assert.That(tile, Has.Property(nameof(Tile.Terrain)).EqualTo(Terrain.Road));
+            Assert.That(tile, Has.Property(nameof(Tile.OccupyingPiece))
+                .TypeOf(new Barricade().GetType()));
+
+        }
+    }
+}
