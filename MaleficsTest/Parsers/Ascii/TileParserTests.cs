e35edbf2252eef5d68503d1d2adcc792eb26b71c
Dennis Renz <dennis@r-renz.de>
2021-01-12 20:46:31 +0100

    Improve test assertion readability

--- MaleficsTest/Parsers/Ascii/TileParserTests.cs
+++ MaleficsTest/Parsers/Ascii/TileParserTests.cs
@@ -21,16 +21,17 @@ namespace MaleficsTest.Parsers.Ascii
         public void A_Barricade_Is_Parsed_With_A_Road_Tile()
         {
             var tile = _parser.Parse('o');
-            Assert.That(tile, Has.Property(nameof(Tile.Terrain)).EqualTo(Terrain.Road));
-            Assert.That(tile, Has.Property(nameof(Tile.OccupyingPiece))
-                .TypeOf(new Barricade().GetType()));
+            
+            Assert.That(tile.Terrain, Is.EqualTo(Terrain.Road));
+            Assert.That(tile.OccupyingPiece, Is.InstanceOf<Barricade>());
         }
 
         [Test]
         public void A_Red_Pawn_Is_Parsed_With_A_Road_Tile()
         {
             var tile = _parser.Parse('r');
-            Assert.That(tile, Has.Property(nameof(Tile.Terrain)).EqualTo(Terrain.Road));
+
+            Assert.That(tile.Terrain, Is.EqualTo(Terrain.Road));
             Assert.That(tile.OccupyingPiece, Is.InstanceOf<Pawn>()
                 .And.Matches<Pawn>(p => p.Player == Player.Red));
         }
