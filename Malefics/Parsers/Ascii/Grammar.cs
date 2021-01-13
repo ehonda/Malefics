c783d60e91d46b9c41c4e8afe4a82795a9374530
Dennis Renz <dennis@r-renz.de>
2021-01-13 20:06:39 +0100

    Introduce constants for single char parsers

--- Malefics/Parsers/Ascii/Grammar.cs
+++ Malefics/Parsers/Ascii/Grammar.cs
@@ -6,16 +6,21 @@ namespace Malefics.Parsers.Ascii
 {
     public static class Grammar
     {
+        private const char ROCK = ' ';
+        private const char ROAD = '.';
+        private const char BARRICADE = 'o';
+        private const char PAWN_RED = 'r';
+
         public static readonly Parser<Tile> Rock
-            = Parse.Char(' ').Return(Tile.Rock());
+            = Parse.Char(ROCK).Return(Tile.Rock());
 
         public static readonly Parser<Tile> Road
-            = Parse.Char('.').Return(Tile.Road());
+            = Parse.Char(ROAD).Return(Tile.Road());
 
         public static readonly Parser<Tile> Barricade
-            = Parse.Char('o').Return(Tile.Barricade());
+            = Parse.Char(BARRICADE).Return(Tile.Barricade());
 
         public static readonly Parser<Tile> RedPawn
-            = Parse.Char('r').Return(Tile.Pawn(Player.Red));
+            = Parse.Char(PAWN_RED).Return(Tile.Pawn(Player.Red));
     }
 }
