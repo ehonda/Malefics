54ddfa682d9680e9669f81a067673b8ca00178de
Dennis Renz <dennis@r-renz.de>
2021-01-14 19:22:05 +0100

    Replace leftover BoardParser.Parse calls

--- MaleficsTest/Parsers/Ascii/BoardParserTests.cs
+++ MaleficsTest/Parsers/Ascii/BoardParserTests.cs
@@ -1,24 +1,17 @@
 ﻿using Malefics.Parsers.Ascii;
 using NUnit.Framework;
+using Sprache;
 
 namespace MaleficsTest.Parsers.Ascii
 {
     [TestFixture]
     public class BoardParserTests
     {
-        private BoardParser _parser = new();
-
-        [SetUp]
-        public void Setup()
-        {
-            _parser = new();
-        }
-
         [Test]
         public void The_Empty_String_Is_Parsed_As_All_Rock_Tiles()
         {
             var asciiBoard = "";
-            var board = _parser.Parse(asciiBoard);
+            var board = Grammar.Board.Parse(asciiBoard);
             Assert.False(board.IsTraversable(new(0, 0)));
         }
 
@@ -26,7 +19,7 @@ namespace MaleficsTest.Parsers.Ascii
         public void An_Unoccupied_Road_Tile_Is_Traversable()
         {
             var asciiBoard = ".";
-            var board = _parser.Parse(asciiBoard);
+            var board = Grammar.Board.Parse(asciiBoard);
             Assert.True(board.IsTraversable(new(0, 0)));
         }
     }
