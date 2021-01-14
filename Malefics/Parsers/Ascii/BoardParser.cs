ee1417c167240d320e5b605a6e4e537a68cf6509
Dennis Renz <dennis@r-renz.de>
2021-01-14 19:17:47 +0100

    Clean up BoardParser file

--- Malefics/Parsers/Ascii/BoardParser.cs
+++ Malefics/Parsers/Ascii/BoardParser.cs
@@ -1,30 +1,12 @@
 ﻿using Malefics.Models;
 using Sprache;
-using System;
-using System.Linq;
 
 namespace Malefics.Parsers.Ascii
 {
     public class BoardParser
     {
-        private readonly string ROW_END = "\n";
-        
-        private readonly TileParser _tileParser = new();
-
         public Board Parse(string board)
         {
-            //var rows = board.Split(ROW_END);
-
-            //var rows = Grammar.BoardRow.Parse(board);
-
-            //var rows = Sprache.Parse.Many(Grammar.BoardRow).Parse(board);
-
-            //var nodePositions = rows
-            //    .Reverse()
-            //    .SelectMany((row, y) => row
-            //        .Select((tile, x) => (new Models.Position(x, y), tile)));
-
-            //return new(nodePositions);
             return Grammar.Board.Parse(board);
         }
     }
