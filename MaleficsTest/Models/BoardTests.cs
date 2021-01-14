ddcc767bb92a5c4fc92b2416f749dd975a67c0e0
Dennis Renz <dennis@r-renz.de>
2021-01-14 19:20:18 +0100

    Replace BoardParser.Parse calls

--- MaleficsTest/Models/BoardTests.cs
+++ MaleficsTest/Models/BoardTests.cs
@@ -2,6 +2,7 @@ using Malefics.Extensions;
 using Malefics.Models;
 using Malefics.Parsers.Ascii;
 using NUnit.Framework;
+using Sprache;
 
 namespace MaleficsTest
 {
@@ -9,33 +10,31 @@ namespace MaleficsTest
     public class BoardTests
     {
         private Board _board = new();
-        private BoardParser _parser = new();
 
         [SetUp]
         public void Setup()
         {
             _board = new();
-            _parser = new();
         }
 
         [Test]
         public void A_Straight_Path_Over_Unoccupied_Road_Tiles_Is_Legal()
         {
-            _board = _parser.Parse("..");
+            _board = Grammar.Board.Parse("..");
             Assert.True(_board.IsLegalPath(Path.AxisParallel(new(0, 0), new(1, 0))));
         }
 
         [Test]
         public void A_Straight_Path_Over_A_Rock_Tile_Is_Not_Legal()
         {
-            _board = _parser.Parse(". .");
+            _board = Grammar.Board.Parse(". .");
             Assert.False(_board.IsLegalPath(Path.AxisParallel(new(0, 0), new(2, 0))));
         }
 
         [Test]
         public void A_Path_Over_Road_Tiles_Around_A_Corner_Is_Legal()
         {
-            _board = _parser.Parse(string.Join('\n',
+            _board = Grammar.Board.Parse(string.Join('\n',
                 "  .",
                 "  .",
                 "..."));
@@ -47,7 +46,7 @@ namespace MaleficsTest
         [Test]
         public void A_Path_Passing_Over_A_Barricade_Is_Not_Legal()
         {
-            _board = _parser.Parse("..o.");
+            _board = Grammar.Board.Parse("..o.");
 
             Assert.False(_board.IsLegalPath(
                 Path.AxisParallel(new(0, 0), new(0, 3))));
@@ -56,7 +55,7 @@ namespace MaleficsTest
         [Test]
         public void A_Path_Over_Road_Tiles_That_Backtracks_Is_Not_Legal()
         {
-            _board = _parser.Parse("...");
+            _board = Grammar.Board.Parse("...");
 
             Assert.False(_board.IsLegalPath(
                 Path.AxisParallelSegments(new(0, 0), new(2, 0), new(1, 0))));
