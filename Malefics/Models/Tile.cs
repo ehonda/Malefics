3e6c2569c9ab6220aa08836f99b897664eed1980
Dennis Renz <dennis@r-renz.de>
2021-01-16 00:13:11 +0100

    Remove direct calls to list methods

--- Malefics/Models/Tile.cs
+++ Malefics/Models/Tile.cs
@@ -1,4 +1,5 @@
-﻿using Malefics.Enums;
+﻿using System;
+using Malefics.Enums;
 using Malefics.Models.Pieces;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,21 @@ namespace Malefics.Models
 
         private IList<IPiece> _occupyingPieces = new List<IPiece>();
 
-        public IList<IPiece> OccupyingPieces 
+        public IList<IPiece> OccupyingPieces
         {
             get => _occupyingPieces;
             init => _occupyingPieces = value.ToList();
         }
 
         public void Add(IPiece piece)
-            => _occupyingPieces.Add(piece);
+        {
+            if (Terrain is Terrain.Rock)
+                throw new InvalidOperationException("Can't add piece to a rock tile.");
+
+            _occupyingPieces.Add(piece);
+        }
+
+        public void RemoveFirst() => _occupyingPieces.RemoveAt(0);
 
         public bool IsOccupied() => OccupyingPieces.Any();
 
--- MaleficsTest/Models/TileTests.cs
+++ MaleficsTest/Models/TileTests.cs
@@ -3,26 +3,25 @@ using Malefics.Models;
 using Malefics.Models.Pieces;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 
 namespace MaleficsTests.Models
 {
     [TestFixture]
     public class TileTests
     {
-        //[Test]
-        //public void No_Piece_Can_Be_Added_To_A_Tile_With_Rock_Terrain()
-        //{
-        //    var tile = Tile.Rock();
-        //    Assert.Catch<InvalidOperationException>(
-        //        () => tile.OccupyingPieces.Add(new Barricade()));
-        //}
+        [Test]
+        public void No_Piece_Can_Be_Added_To_A_Tile_With_Rock_Terrain()
+        {
+            var tile = Tile.Rock();
+            Assert.Catch<InvalidOperationException>(
+                () => tile.Add(new Barricade()));
+        }
 
         [Test]
         public void A_Pawn_Can_Be_Added_To_An_Empty_House()
         {
             var house = new Tile { Terrain = Terrain.House };
-            house.OccupyingPieces.Add(new Pawn { Player = Player.Red });
+            house.Add(new Pawn { Player = Player.Red });
 
             Assert.That(house.OccupyingPieces, Has.Exactly(1).Items);
         }
@@ -35,7 +34,7 @@ namespace MaleficsTests.Models
                 Terrain = Terrain.House, 
                 OccupyingPieces = new[] { new Pawn { Player = Player.Red } }
             };
-            house.OccupyingPieces.RemoveAt(0);
+            house.RemoveFirst();
 
             Assert.That(house.OccupyingPieces, Is.Empty);
         }
