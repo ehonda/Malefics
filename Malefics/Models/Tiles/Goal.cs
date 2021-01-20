f0ac45af60a436b7930c6b602bf3498211a64977
Dennis Renz <dennis@r-renz.de>
2021-01-20 19:46:49 +0100

    Add Goal with canonical implementations

---
+++ Malefics/Models/Tiles/Goal.cs
@@ -0,0 +1,52 @@
+﻿using Malefics.Models.Pieces;
+
+namespace Malefics.Models.Tiles
+{
+    public class Goal : ITile
+    {
+        #region Implementation of ITile
+
+        /// <inheritdoc />
+        public bool IsTraversable()
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        public bool Contains(Piece piece)
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void Put(Piece piece)
+        {
+        }
+
+        /// <inheritdoc />
+        public Piece Take()
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        public Piece? Peek()
+        {
+            return null;
+        }
+
+        /// <inheritdoc />
+        public bool IsOccupied()
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        public bool IsValidCaptureTargetFor(Piece piece)
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
