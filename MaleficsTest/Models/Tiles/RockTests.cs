d6310d8766c4b21d60741cd13791bbe08605f482
Dennis Renz <dennis@r-renz.de>
2021-01-21 18:53:25 +0100

    Refactor rock tests

--- MaleficsTest/Models/Tiles/RockTests.cs
+++ MaleficsTest/Models/Tiles/RockTests.cs
@@ -9,36 +9,41 @@ namespace MaleficsTests.Models.Tiles
     [TestFixture]
     public class RockTests
     {
+        private Rock _rock = new();
+
+        [SetUp]
+        public void SetUp() => _rock = new();
+
         [Test]
         public void No_Piece_Can_Be_Put_On_A_Rock_Tile()
             => Assert.Catch<InvalidTileOperationException>(
-                () => Tile.Rock().Put(new Barricade()));
+                () => _rock.Put(new Barricade()));
 
         [Test]
         public void A_Rock_Can_Not_Be_Taken_From()
             => Assert.Catch<InvalidTileOperationException>(
-                () => Tile.Rock().Take());
+                () => _rock.Take());
 
         [Test]
         public void A_Rock_Tile_Is_Not_Traversable()
-            => Assert.That(Tile.Rock().IsTraversable, Is.False);
+            => Assert.That(_rock.IsTraversable, Is.False);
 
         [Test]
         public void A_Rock_Is_Never_Occupied()
-            => Assert.That(Tile.Rock().IsOccupied, Is.False);
+            => Assert.That(_rock.IsOccupied, Is.False);
 
         [Test]
         [TestCaseSource(typeof(PieceCases), nameof(PieceCases.AllPieces))]
         public void A_Rock_Is_Not_A_Valid_Capture_Target_For_Any_Piece(Piece piece)
-            => Assert.That(Tile.Rock().IsValidCaptureTargetFor(piece), Is.False);
+            => Assert.That(_rock.IsValidCaptureTargetFor(piece), Is.False);
 
         [Test]
         [TestCaseSource(typeof(PieceCases), nameof(PieceCases.AllPieces))]
         public void A_Rock_Contains_No_Piece(Piece piece)
-            => Assert.That(Tile.Rock().Contains(piece), Is.False);
+            => Assert.That(_rock.Contains(piece), Is.False);
 
         [Test]
         public void Peeking_At_A_Rock_Returns_Null()
-            => Assert.That(Tile.Rock().Peek(), Is.Null);
+            => Assert.That(_rock.Peek(), Is.Null);
     }
 }
