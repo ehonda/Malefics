bcec08523d68a6a3d5a36228824c07f186b83608
Dennis Renz <dennis@r-renz.de>
2021-01-21 19:24:30 +0100

    Add Goal Tests

---
+++ MaleficsTest/Models/Tiles/GoalTests.cs
@@ -0,0 +1,13 @@
+﻿using Malefics.Models.Tiles;
+using NUnit.Framework;
+
+namespace MaleficsTests.Models.Tiles
+{
+    [TestFixture]
+    public class GoalTests
+    {
+        [Test]
+        public void A_Goal_Is_Traversable()
+            => Assert.That(new Goal().IsTraversable, Is.True);
+    }
+}
