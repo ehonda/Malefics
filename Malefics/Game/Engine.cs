eac05b29ea6721a1a8aaac6e0684e1a8164731ca
Dennis Renz <dennis@r-renz.de>
2021-01-27 20:07:42 +0100

    Add first engine test with mock players

--- Malefics/Game/Engine.cs
+++ Malefics/Game/Engine.cs
@@ -1,6 +1,25 @@
-﻿namespace Malefics.Game
+﻿using Malefics.Models;
+using Malefics.Players;
+
+namespace Malefics.Game
 {
     public class Engine
     {
+        private readonly Board _board;
+        private readonly IPlayer[] _players;
+
+        public Engine(Board board, IPlayer[] players)
+        {
+            _board = board;
+            _players = players;
+        }
+
+        public void Run()
+        {
+            foreach (var player in _players)
+            {
+                player.RequestPawnMove(_board, 0);
+            }
+        }
     }
 }
--- MaleficsTest/Game/EngineTests.cs
+++ MaleficsTest/Game/EngineTests.cs
@@ -1,9 +1,41 @@
-﻿using NUnit.Framework;
+﻿using Malefics.Game;
+using Malefics.Models;
+using Malefics.Players;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
 
 namespace MaleficsTests.Game
 {
     [TestFixture]
     public class EngineTests
     {
+        [Test]
+        public void The_Engine_Requests_Moves_From_Every_Player()
+        {
+            var red = new Mock<IPlayer>();
+            var blue = new Mock<IPlayer>();
+
+            red.Setup(p => p.RequestPawnMove(
+                    It.IsAny<Board>(),
+                    It.IsAny<uint>()))
+                .Returns(Enumerable.Empty<Position>());
+
+            blue.Setup(p => p.RequestPawnMove(
+                    It.IsAny<Board>(),
+                    It.IsAny<uint>()))
+                .Returns(Enumerable.Empty<Position>());
+
+            var engine = new Engine(new(), new[] {red.Object, blue.Object});
+            engine.Run();
+
+            red.Verify(p => p.RequestPawnMove(
+                It.IsAny<Board>(),
+                It.IsAny<uint>()), Times.AtLeastOnce);
+
+            blue.Verify(p => p.RequestPawnMove(
+                It.IsAny<Board>(),
+                It.IsAny<uint>()), Times.AtLeastOnce);
+        }
     }
 }
