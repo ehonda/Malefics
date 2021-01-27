2c0475ff08514b284d6c73da103159c7e2fcad95
Dennis Renz <dennis@r-renz.de>
2021-01-27 19:52:49 +0100

    Add IPlayer

---
+++ Malefics/Players/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Malefics.Enums;
+using Malefics.Models;
+
+namespace Malefics.Players
+{
+    public interface IPlayer
+    {
+        public PlayerColor PlayerColor { get; init; }
+
+        // TODO: Replace Board access by something like a "view" access
+        public IEnumerable<Position> RequestPawnMove(Board board, uint pips);
+    }
+}
