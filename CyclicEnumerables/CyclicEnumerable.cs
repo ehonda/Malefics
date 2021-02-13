846f0874e34146bd6eaec2ba70e7b5e615c7155f
Dennis Renz <dennis@r-renz.de>
2021-02-13 11:05:41 +0100

    Add CyclicEnumerables project
    - This is a temporary solution, we'd rather release this as nuget and
      use that nuget to implement our cyclic mocks

---
+++ CyclicEnumerables/CyclicEnumerable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CyclicEnumerables
+{
+    public class CyclicEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> _base;
+
+        public CyclicEnumerable(IEnumerable<T> @base) => _base = @base;
+
+        /// <inheritdoc />
+        public IEnumerator<T> GetEnumerator() => new CyclicEnumerator<T>(_base.GetEnumerator());
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ CyclicEnumerables/CyclicEnumerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CyclicEnumerables
+{
+    // TODO: Publish this whole project as a nuget, use the nuget in MaleficsTests
+    public class CyclicEnumerator<T> : IEnumerator<T>
+    {
+        private readonly IEnumerator<T> _base;
+
+        public CyclicEnumerator(IEnumerator<T> @base) => _base = @base;
+
+        /// <inheritdoc />
+        public bool MoveNext()
+        {
+            if (_base.MoveNext() is false)
+            {
+                Reset();
+                if (_base.MoveNext() is false) return false;
+            }
+
+            Current = _base.Current;
+            return true;
+        }
+
+        /// <inheritdoc />
+        public void Reset()
+        {
+            _base.Reset();
+        }
+
+        // CS8766 is wrong here - IEnumerator<T>.Current has [CanBeNull]
+        /// <inheritdoc />
+        public T? Current { get; private set; }
+
+        /// <inheritdoc />
+        object? IEnumerator.Current => Current;
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _base.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
