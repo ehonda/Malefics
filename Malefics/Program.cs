b68cb6a6c1926128b9c37d8387186cc2b2d57874
Dennis Renz <dennis@r-renz.de>
2021-02-13 20:08:19 +0100

    Add some static board states for demo

--- Malefics/Program.cs
+++ Malefics/Program.cs
@@ -1,4 +1,5 @@
-﻿using Malefics.Enums;
+﻿using System.IO;
+using Malefics.Enums;
 using Malefics.Models.Pieces;
 using Malefics.Models.Tiles;
 using Malefics.Parsers.Ascii;
@@ -11,11 +12,17 @@ namespace Malefics
     {
         static void Main(string[] args)
         {
+            //            var board = Grammar.Board().Parse(
+            //@"r...x..b.
+            //.       .
+            //.........
+            // R2    B2");
+
             var board = Grammar.Board().Parse(
-@"r...x..b.
-.       .
-.........
- R2    B2");
+                File.ReadAllText(@"C:\Users\dennis\source\repos\CSharp\Malefics\Malefics\boards\standard_malefiz_some_turns_played.txt"));
+
+            //var board = Grammar.Board().Parse(
+            //    File.ReadAllText(@"C:\Users\dennis\source\repos\CSharp\Malefics\Malefics\boards\standard_malefiz.txt"));
 
             AnsiConsole.Render(new Panel(board));
             //AnsiConsole.Render(board);
