99f261e1c418c75eea87297a524be0989ba78d42
Dennis Renz <dennis@r-renz.de>
2021-02-18 18:36:51 +0100

    Add random AI

---
+++ Malefics/Players/AI/Random.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Malefics.Enums;
+using Malefics.Models;
+
+namespace Malefics.Players.AI
+{
+    public class Random : IPlayer
+    {
+        #region Implementation of IPlayer
+
+        /// <inheritdoc />
+        public PlayerColor PlayerColor { get; init; }
+
+        /// <inheritdoc />
+        public IEnumerable<Position> RequestPawnMove(Board board, uint pips)
+        {
+            yield break;
+        }
+
+        /// <inheritdoc />
+        public Position RequestBarricadePlacement(Board board) => null;
+
+        #endregion
+    }
+}
